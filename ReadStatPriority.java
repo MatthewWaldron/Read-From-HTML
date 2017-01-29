6a2e96f6d66796af8bf8f5070673e4de0efd711e
MatthewWaldron <mwaldron777@gmail.com>
2017-01-29 17:44:59 -0500

    Exploring methods to reduce the amount of user input

--- ReadStatPriority.java
+++ ReadStatPriority.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
@@ -12,12 +13,33 @@ public class ReadStatPriority {
 	{
 		Scanner read = new Scanner(System.in);
 		
-		System.out.println("Enter the class:");
+		String[] healers = {"restoration", "mistweaver", "holy", "discipline"};
+		String[] tanks = {"blood", "protection", "guardian", "brewmaster", "vengeance"};
+		String[] dpsOnlyClass = {"mage", "hunter", "warlock", "rogue"};
+		String[] dps = {"frost", "unholy", "havoc", "balance", "feral", "windwalker", "retribution", "shadow", "elemental", "enhancement", "arms", "fury"};
+		
+		System.out.println("Please enter a class to recieve stat priority:");
 		String wowClass = read.nextLine();
 		System.out.println("Enter the specialization:");
 		String wowSpec = read.nextLine();
-		System.out.println("Enter the role (dps, tank, healer)");
-		String wowRole = read.nextLine();
+		String wowRole = "";
+		
+		if(Arrays.asList(dpsOnlyClass).contains(wowClass))
+		{
+			wowRole = "dps";
+		} 
+		else if (Arrays.asList(healers).contains(wowSpec))
+		{
+			wowRole = "healer";
+		}
+		else if (Arrays.asList(tanks).contains(wowSpec))
+		{
+			wowRole = "tank";
+		}
+		else if (Arrays.asList(dps).contains(wowSpec))
+		{
+			wowRole = "dps";
+		}
 		
 		String inputUrl = wowSpec.toLowerCase() + "-" + wowClass.toLowerCase().replaceAll(" ", "-") + "-pve-" + wowRole.toLowerCase(); 
 		String url = "http://www.icy-veins.com/wow/" + inputUrl + "-stat-priority";
