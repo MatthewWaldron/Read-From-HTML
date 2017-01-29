c413b0a4aca2f93012e25e30e5a07a538079ba56
MatthewWaldron <mwaldron777@gmail.com>
2017-01-29 17:36:35 -0500

    Added the ability for the user to provide input

--- ReadStatPriority.java
+++ ReadStatPriority.java
@@ -1,21 +1,34 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ReadStatPriority {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException 
 	{
-		String url = "http://www.icy-veins.com/wow/fury-warrior-pve-dps-stat-priority";
+		Scanner read = new Scanner(System.in);
+		
+		System.out.println("Enter the class:");
+		String wowClass = read.nextLine();
+		System.out.println("Enter the specialization:");
+		String wowSpec = read.nextLine();
+		System.out.println("Enter the role (dps, tank, healer)");
+		String wowRole = read.nextLine();
+		
+		String inputUrl = wowSpec.toLowerCase() + "-" + wowClass.toLowerCase().replaceAll(" ", "-") + "-pve-" + wowRole.toLowerCase(); 
+		String url = "http://www.icy-veins.com/wow/" + inputUrl + "-stat-priority";
 		Document htmlDoc = Jsoup.connect(url).get();
 		//Stat priority and weight are organized using the ol tag
 		Elements priority = htmlDoc.select("ol");
 		
 		String scale = priority.text();
-		String[] result = scale.split(";");
+		String[] result = scale.split("; ");
+		System.out.println();
+		
 		for(String s : result)
 		{
 			System.out.println(s);
