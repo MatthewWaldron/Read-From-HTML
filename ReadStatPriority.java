1d455327de83ce4b7545891d38337d576da5c175
MatthewWaldron <mwaldron777@gmail.com>
2017-01-29 17:57:18 -0500

    Moved processes to a new method

--- ReadStatPriority.java
+++ ReadStatPriority.java
@@ -12,50 +12,70 @@ public class ReadStatPriority {
 	public static void main(String[] args) throws IOException 
 	{
 		Scanner read = new Scanner(System.in);
+
+		System.out.println("Please enter a class to recieve stat priority:");
+		String wowClass = read.nextLine();
+		System.out.println("Enter the specialization:");
+		String wowSpec = read.nextLine();
+		String wowRole = checkRole(wowClass, wowSpec);
+	
 		
+		if(wowRole != null)
+		{
+			String inputUrl = wowSpec.toLowerCase() + "-" + wowClass.toLowerCase().replaceAll(" ", "-") + "-pve-" + wowRole.toLowerCase(); 
+			String url = "http://www.icy-veins.com/wow/" + inputUrl + "-stat-priority";
+			Document htmlDoc = null;
+			try{
+				htmlDoc = Jsoup.connect(url).get();
+			} catch (org.jsoup.HttpStatusException e)
+			{
+				System.err.println("Unable to find http address: " + e.getMessage());
+			}
+			if(htmlDoc != null)
+			{
+				//Stat priority and weight are organized using the ol tag
+				Elements priority = htmlDoc.select("ol");
+				
+				String scale = priority.text();
+				String[] result = scale.split("; ");
+				System.out.println();
+				
+				for(String s : result)
+				{
+					System.out.println(s);
+				}
+			}
+		}
+		else
+		{
+			System.out.println("Cannot find class/spec combination, please try again.");
+		}
+	}
+	
+	private static String checkRole(String wowClass, String wowSpec)
+	{
 		String[] healers = {"restoration", "mistweaver", "holy", "discipline"};
 		String[] tanks = {"blood", "protection", "guardian", "brewmaster", "vengeance"};
 		String[] dpsOnlyClass = {"mage", "hunter", "warlock", "rogue"};
 		String[] dps = {"frost", "unholy", "havoc", "balance", "feral", "windwalker", "retribution", "shadow", "elemental", "enhancement", "arms", "fury"};
 		
-		System.out.println("Please enter a class to recieve stat priority:");
-		String wowClass = read.nextLine();
-		System.out.println("Enter the specialization:");
-		String wowSpec = read.nextLine();
-		String wowRole = "";
-		
 		if(Arrays.asList(dpsOnlyClass).contains(wowClass))
 		{
-			wowRole = "dps";
+			return "dps";
 		} 
 		else if (Arrays.asList(healers).contains(wowSpec))
 		{
-			wowRole = "healer";
+			return "healer";
 		}
 		else if (Arrays.asList(tanks).contains(wowSpec))
 		{
-			wowRole = "tank";
+			return "tank";
 		}
 		else if (Arrays.asList(dps).contains(wowSpec))
 		{
-			wowRole = "dps";
+			return "dps";
 		}
-		
-		String inputUrl = wowSpec.toLowerCase() + "-" + wowClass.toLowerCase().replaceAll(" ", "-") + "-pve-" + wowRole.toLowerCase(); 
-		String url = "http://www.icy-veins.com/wow/" + inputUrl + "-stat-priority";
-		Document htmlDoc = Jsoup.connect(url).get();
-		//Stat priority and weight are organized using the ol tag
-		Elements priority = htmlDoc.select("ol");
-		
-		String scale = priority.text();
-		String[] result = scale.split("; ");
-		System.out.println();
-		
-		for(String s : result)
-		{
-			System.out.println(s);
-		}
-		
+		return null;
 	}
 
 }
